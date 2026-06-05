4b9e133420b6bc169065b42495bc4533637277d2
Nader Garas <test@gmail.com>
2026-06-05 16:02:02 -0400

    Added more to the algorithms page, doesn't use alerts to show Algorithms, added styling to css

--- frontend/src/data/algorithms.js
+++ frontend/src/data/algorithms.js
@@ -15,12 +15,65 @@ const PLL = [
 	},
 ];
 
-const OLL = [];
-const F2L = [];
-const CMLL = [];
-const LSE = [];
-const CLL = [];
-const Ortega = [];
+const OLL = [
+	{
+		id: "Sune",
+		alg: "R U R' U R U2 R'",
+		visual: "img",
+		notes:
+			"One of the most common OLL cases, it is also the first one that most people learn. It is also the only OLL case that can be solved in 7 moves or less",
+	},
+];
+
+const F2L = [
+	{
+		id: "F2L case 1",
+		alg: "U R U' R'",
+		visual: "img",
+		notes:
+			"One of the most basic F2L cases, it is also the first one that most people learn. It is also the only F2L case that can be solved in 4 moves or less",
+	},
+];
+
+const CMLL = [
+	{
+		id: "A-perm",
+		alg: "x R' U R' D2 R U' R' D2 R2 x'",
+		visual: "img",
+		notes:
+			"One of the most common CMLL cases, it is also the first one that most people learn. It is also the only CMLL case that can be solved in 8 moves or less",
+	},
+];
+
+const LSE = [
+	{
+		id: "H-perm",
+		alg: "M2 U M2 U2 M2 U M2",
+		visual: "img",
+		notes:
+			"One of the most common LSE cases, it is also the first one that most people learn. It is also the only LSE case that can be solved in 6 moves or less",
+	},
+];
+
+const CLL = [
+	{
+		id: "A-perm",
+		alg: "x R' U R' D2 R U' R' D2 R2 x'",
+		visual: "img",
+		notes:
+			"One of the most common CLL cases, it is also the first one that most people learn. It is also the only CLL case that can be solved in 8 moves or less",
+	},
+];
+
+const Ortega = [
+	{
+		id: "Ortega case 1",
+		alg: "R U R' U R U2 R'",
+		visual: "img",
+		notes:
+			"One of the most common Ortega cases, it is also the first one that most people learn. It is also the only Ortega case that can be solved in 7 moves or less",
+	},
+];
 
 const Algorithms = {
 	PLL: PLL,
--- frontend/src/pages/AlgorithmsPage.jsx
+++ frontend/src/pages/AlgorithmsPage.jsx
@@ -2,10 +2,17 @@ import AlgorithmNav from "../components/AlgorithmNav";
 import Algorithms from "../data/algorithms.js";
 
 function AlgorithmsPage({ category }) {
-	alert(Algorithms[category]);
 	return (
 		<main className="main-panel">
 			<AlgorithmNav />
+			<div className="algorithms-container">
+				{Algorithms[category].map((algcase) => (
+					<div className="algorithm-card">
+						<h2>{algcase.id}</h2>
+						<h4>{algcase.alg}</h4>
+					</div>
+				))}
+			</div>
 		</main>
 	);
 }
